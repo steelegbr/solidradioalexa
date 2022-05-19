84d29503dcca68a4700d43c3c91183931f845e21
Marc Steele <steelegbr@gmail.com>
2022-05-19 16:41:32 +0100

    Update copyright.

--- lambda/index.js
+++ lambda/index.js
@@ -1,6 +1,6 @@
 /**
     Solid Radio Alexa Skill
-    Copyright (C) 2019-2021 Marc Steele
+    Copyright (C) 2019-2022 Marc Steele
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
